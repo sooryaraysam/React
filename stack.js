96b9c89dfb37a4222b0afe9bf2c466838ff09ae6
--add <--add>
2022-02-22 18:04:29 +0100

    Stack implementation stage 1

---
+++ stack.js
@@ -0,0 +1,71 @@
+class Stack
+{
+top;
+array;
+total_size;
+
+constructor(total_size)
+{
+    this.array=[]
+    this.top = -1;
+    this.total_size = total_size;             
+}
+
+
+push(ele)    
+{
+if(this.top==this.total_size-1)
+{
+    console.log("Cannot insert! Stack overflow")
+}
+else
+{
+    this.top += 1;
+    this.array[this.top] = ele;
+    console.log(`The element being pushed is ${ele}`)
+    return this.top;
+}
+}
+
+pop()
+{
+if(this.top==-1)
+console.log("underfow!!");
+
+else
+{
+    console.log("Initiating Stack pop process ")
+    console.log(`${this.array[this.top]} is popped`)
+    this.top=this.top-1;
+    
+    console.log("Pop process ends ")
+    return this.top;       
+}
+}
+
+peek()
+{
+if(this.top == -1)
+{
+    console.log("Stack is empty")
+}
+else
+{   
+    console.log("The top element of the Stack is " + this.array[this.top]);   
+}
+}
+
+}
+
+
+let stack = new Stack(3);
+stack.push(5);
+stack.push(6);
+stack.pop()
+stack.peek();
+
+
+
+
+
+
