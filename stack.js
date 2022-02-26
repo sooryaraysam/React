83b610ebfa022b650839009bf0064b2f34650141
sooryaraysam <sooryaraysam@gmail.com>
2022-02-26 20:51:45 +0100

    Stack class created

--- stack.js
+++ stack.js
@@ -10,59 +10,9 @@ constructor(total_size)
     this.top = -1;
     this.total_size = total_size;             
 }
-
-push(ele)    
-{
-if(this.top==this.total_size-1)
-{
-    console.log("Cannot insert! Stack overflow")
-}
-else
-{
-    this.top = this.top + 1;
-    this.array[this.top] = ele;
-    console.log(`The element being pushed is ${ele}`)
-    return this.top;
-}
-}
-
-pop()
-{
-if(this.top==-1)
-console.log("underfow!!");
-
-else
-{
-    console.log("Initiating Stack pop process ")
-    console.log(`${this.array[this.top]} is popped`)
-    this.top=this.top-1;
-    
-    console.log("Pop process ends ")
-    return this.top;       
-}
-}
-peek()
-{
-if(this.top == -1)
-{
-    console.log("Stack is empty")
-}
-else
-{   
-    console.log("The top element of the Stack is " + this.array[this.top]);   
-}
-}
-
 }
 
 
-let stack = new Stack(1);
-
-
-stack.push(5);
-stack.push(6);
-stack.pop()
-stack.peek();
 
 
 
